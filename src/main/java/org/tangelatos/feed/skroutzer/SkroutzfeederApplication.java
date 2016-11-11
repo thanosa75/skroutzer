2e5b83b12e3ae357595b977a9aaa6afd11ff6b5e
thanos <thanos@dev-thanosa.local>
2016-11-11 23:34:54 +0200

    First

---
+++ src/main/java/org/tangelatos/feed/skroutzer/SkroutzfeederApplication.java
@@ -0,0 +1,18 @@
+package org.tangelatos.feed.skroutzer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkroutzfeederApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkroutzfeederApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+
+	}
+}
